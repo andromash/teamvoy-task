242d481779018a564bc81867c4bcfbf7fc1a9e6e
ROMASHSX <andriiromash@gmail.com>
2020-11-07 21:24:51 +0200

    addItem in controller added

--- src/main/java/testtask/teamvoy/controller/ItemController.java
+++ src/main/java/testtask/teamvoy/controller/ItemController.java
@@ -2,9 +2,12 @@ package testtask.teamvoy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import testtask.teamvoy.dto.ItemRequestDto;
 import testtask.teamvoy.dto.ItemResponseDto;
 import testtask.teamvoy.mappers.ItemMapper;
 import testtask.teamvoy.model.Item;
@@ -30,5 +33,10 @@ public class ItemController {
         return itemService.getAllNeededCheapItem(name, quantity).stream()
                 .map(itemMapper::mapItemTODto)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void addItem(@RequestBody ItemRequestDto itemRequestDto) {
+        itemService.save(itemMapper.mapDtoToItem(itemRequestDto));
     }
 }
